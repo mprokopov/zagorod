57839693154c4845d2225c590b19683ebac7b1d0
Max Prokopov <mprokopov@gmail.com>
2011-06-25 15:38:04 +0300

    подготовка к хостингу

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,9 +16,38 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 set :user, "nexus"
 
-role :app, "colo2.it-link.com.ua"
-role :web, "colo2.it-link.com.ua"
-role :db,  "colo2.it-link.com.ua", :primary => true
+role :app, "colo.it-premium.com.ua"
+role :web, "colo.it-premium.com.ua"
+role :db,  "colo.it-premium.com.ua", :primary => true
+
+after :deploy, "passenger:restart"
+
+
+set :app_symlinks, %w( photo )
+
+desc "Symlinks the :app_symlinks directories from current/public to shared/public"
+task :symlink_public do
+   if app_symlinks
+     app_symlinks.each do |link|
+       run "ln -nfs #{shared_path}/public/#{link} #{current_path}/public/#{link}"
+     end
+   end
+end
+
+desc "Copy production assets"
+task :copy_assets do
+   download "#{shared_path}/public/assets", "public/assets", :once=>true, :recursive=>true
+end
+
+namespace :deploy do
+   task :after_symlink, :roles => [:app, :web] do
+     symlink_public
+   end
+   desc "Restart Application"
+   task :restart do
+     run "touch #{current_path}/tmp/restart.txt"
+   end
+end
 
 namespace :passenger do
   desc "Restart Application"
@@ -26,5 +55,3 @@ namespace :passenger do
     run "touch #{current_path}/tmp/restart.txt"
   end
 end
-
-after :deploy, "passenger:restart"
